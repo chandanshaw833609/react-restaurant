70a9e09fa21a6f12a6097f8f7403c61d66be5397
chandan <chandanshaw833609@gmail.com>
2024-05-04 05:06:47 +0530

    changed cart icon

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -62,7 +62,7 @@ const Header = () => {
                   to={"/cart"}
                   className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                 >
-                  <ShoppingCartIcon/> OrderBag 
+                  <ShoppingCartIcon/> 
                 </Link>
               ) : (
                 <Link
