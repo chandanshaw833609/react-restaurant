1b98fce840ec4a2d728ec37ccb1bc2d713535a4c
chandan <chandanshaw833609@gmail.com>
2024-05-04 05:40:41 +0530

    fixed login url

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -73,7 +73,7 @@ const Header = () => {
                 </button>
               ) : (
                 <Link
-                  to="/l"
+                  to="/login"
                   className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                 >
                   Login
